d549f1d81e7422832302396fd3d8a9d21bd1f5aa
Anupam Vyshakhi S <115809998+AnupamVyshakhi@users.noreply.github.com>
2024-04-03 22:41:56 +0530

    Add files via upload

---
+++ class_1/assignments/odd_even.py
@@ -0,0 +1,5 @@
+n=int(input("Enter the number"))
+if n%2==0:
+  print("The number '",n,"' is an even number")
+else:
+  print("The number '",n,"' is an odd number")
---
+++ class_1/assignments/positive.py
@@ -0,0 +1,3 @@
+num=int(input("Enter a non zero number"))
+if num>0:
+  print("Number '",num,"' is a positive number!!!")
